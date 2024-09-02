f7854dccc1d62898d47af0fd7e12cc772b59e3b5
Madiyar Segizbay <m.segizbay@aues.kz>
2024-09-02 11:17:33 +0500

    Tutorial GitMapping and RequestParam

--- src/main/java/kz/segizbay/springcourse/controllers/FirstController.java
+++ src/main/java/kz/segizbay/springcourse/controllers/FirstController.java
@@ -1,14 +1,21 @@
 package kz.segizbay.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false ) String surname){
+        System.out.println(name + " " + surname);
         return "first/hello";
     }
 
