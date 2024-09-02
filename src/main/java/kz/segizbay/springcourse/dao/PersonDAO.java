0bc2e2bfff931192344cd9b282330ba9b8282a9f
Madiyar Segizbay <m.segizbay@aues.kz>
2024-09-02 20:06:29 +0500

    CRUD, REST, DAO(Data Access Object)

---
+++ src/main/java/kz/segizbay/springcourse/dao/PersonDAO.java
@@ -0,0 +1,32 @@
+package kz.segizbay.springcourse.dao;
+
+import kz.segizbay.springcourse.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private List<Person> people;
+    private static int PEOPLE_COUNT;
+
+    {
+        people = new ArrayList<Person>();
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "John"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream()
+                .filter(person -> person.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+}
