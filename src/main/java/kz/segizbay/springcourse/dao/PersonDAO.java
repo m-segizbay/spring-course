15c2723debefcb9d25b7b8f956cfcbd018192ed4
Madiyar Segizbay <m.segizbay@aues.kz>
2024-09-03 17:34:51 +0500

    Testing Batch UPDATE ABD Ordinary update

--- src/main/java/kz/segizbay/springcourse/dao/PersonDAO.java
+++ src/main/java/kz/segizbay/springcourse/dao/PersonDAO.java
@@ -2,6 +2,7 @@ package kz.segizbay.springcourse.dao;
 
 import kz.segizbay.springcourse.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -42,5 +43,55 @@ public class PersonDAO {
 
     public void delete(int id) {
         jdbcTemplate.update("DELETE FROM person WHERE id = ?", id);
+    }
+
+
+    // Тестируем производительность пакетной вставки
+
+    public void testMultipleUpdate() {
+        List<Person> people = create1000People();
+        long before = System.currentTimeMillis();
+        for (Person p : people){
+            jdbcTemplate.update("INSERT INTO person VALUES (?, ?, ?, ?)",
+                    p.getId(), p.getName(), p.getAge(), p.getEmail());
+        }
+        long after = System.currentTimeMillis();
+        System.out.println("Standart time: " + (after-before));
+    }
+
+    private List<Person> create1000People() {
+        List<Person> people = new ArrayList<>();
+
+        for (int i = 0; i < 1000; i++){
+            people.add(new Person(i, "Name " + i, 30, "test"+i+"@gmail.com"));
+        }
+
+        return people;
+    }
+
+    public void testBatchUpdate() {
+        List<Person> people = create1000People();
+
+        long before = System.currentTimeMillis();
+
+        jdbcTemplate.batchUpdate("INSERT INTO person VALUES (?, ?, ?, ?)",
+                new BatchPreparedStatementSetter() {
+                    @Override
+                    public void setValues(PreparedStatement preparedStatement, int i) throws SQLException {
+                        preparedStatement.setInt(1, people.get(i).getId());
+                        preparedStatement.setString(2, people.get(i).getName());
+                        preparedStatement.setInt(3, people.get(i).getAge());
+                        preparedStatement.setString(4, people.get(i).getEmail());
+                    }
+
+                    @Override
+                    public int getBatchSize() {
+                        return people.size();
+                    }
+                });
+
+        long after = System.currentTimeMillis();
+
+        System.out.println("Batch time: " + (after-before));
     }
 }
